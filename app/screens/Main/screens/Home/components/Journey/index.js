71e12206f50f4f4102b8ab702b39399d3c1f5ae0
Jazz Turnbull <jdturnbull98@gmail.com>
2023-12-03 13:28:40 +0000

    .

--- app/screens/Main/screens/Home/components/Journey/index.js
+++ app/screens/Main/screens/Home/components/Journey/index.js
@@ -10,6 +10,9 @@ import { getIconFromLabel } from '../../../../../../utils/icon';
 
 const BOX_WIDTH = 140;
 const BOX_HEIGHT = 60;
+const ITEM_HEIGHT = BOX_HEIGHT + 35;
+const TOP_HEIGHT = 100;
+const TAB_HEIGHT = 95;
 
 const Journey = () => {
   const plannedActivities = useSelector((state) => state.user.plannedActivities);
@@ -18,7 +21,8 @@ const Journey = () => {
   const [activeMonth, setActiveMonth] = useState(plannedMonths[0]?.toUpperCase());
   const [startOfWeekDates, setStartOfWeekDates] = useState([]);
   const [visibleIndexs, setVisibleIndexs] = useState([]);
-  const [openModalId, setOpenModalId] = useState();
+  const [modalDetails, setModalDetails] = useState();
+  const [_viewableItems, _setViewableItems] = useState([]);
 
   const [scrollPosition, setScrollPosition] = useState(0);
 
@@ -54,15 +58,17 @@ const Journey = () => {
   }, []);
 
   const viewabilityConfig = {
-    viewAreaCoveragePercentThreshold: 50,
+    viewAreaCoveragePercentThreshold: 10,
   };
 
   const handleLayout = useCallback(() => {
     setActiveMonth(plannedMonths[0]?.toUpperCase());
   }, []);
 
-  const renderItem = (props) => {
+  const ListItem = (props) => {
     const { item, index } = props;
+
+    const buttonRef = useRef(null);
 
     const date = moment.utc(item.date, 'YYYY-MM-DD');
 
@@ -78,21 +84,70 @@ const Journey = () => {
     const label = arr.length === 3 ? `${arr[1]}${arr[2]}` : `${arr[2]}${arr[3]}`;
 
     const handlePress = () => {
-      if (!visibleIndexs.includes(index)) {
-        hapticImpact();
-        const newScrollPosition = scrollPosition + 200;
-        flatListRef.current.scrollToOffset({ offset: newScrollPosition, animated: true });
-      } else if (visibleIndexs.includes(index) && index >= visibleIndexs[visibleIndexs.length - 2]) {
-        hapticImpact();
-        const newScrollPosition = scrollPosition + 200;
-        flatListRef.current.scrollToOffset({ offset: newScrollPosition, animated: true });
+      if (modalDetails?.id === item.id) {
+        setModalDetails(null);
+        return;
       }
 
-      setOpenModalId(item.id);
+      let positionChange = 0;
+
+      if (index < visibleIndexs[0]) {
+        // Item is above the visible items, scroll up
+        console.log('Item is above the visible items, scroll up');
+        flatListRef.current.scrollToOffset({ offset: scrollPosition - ITEM_HEIGHT, animated: true });
+        positionChange = -ITEM_HEIGHT;
+      } else if (index > visibleIndexs[visibleIndexs.length - 1]) {
+        // Item is below the visible items, scroll down
+        console.log('Item is below the visible items, scroll down');
+        flatListRef.current.scrollToOffset({ offset: scrollPosition + ITEM_HEIGHT, animated: true });
+        positionChange = ITEM_HEIGHT;
+      } else if (index === visibleIndexs[visibleIndexs.length - 1]) {
+        // Item is the last visible item, scroll down
+        console.log('Item is the last visbible item, scroll down');
+        flatListRef.current.scrollToOffset({ offset: scrollPosition + ITEM_HEIGHT, animated: true });
+        positionChange = ITEM_HEIGHT;
+      } else if (index === visibleIndexs[0] && scrollPosition > 99) {
+        // Item is the first visible item, scroll up
+        console.log('Item is the first visbible item, scroll up');
+        flatListRef.current.scrollToOffset({ offset: scrollPosition - ITEM_HEIGHT, animated: true });
+        positionChange = -ITEM_HEIGHT;
+      }
+
+      const NUM_SEPARATORS = startOfWeekDates.filter((date) => {
+        return moment.utc(date).isBefore(item.date);
+      }).length;
+
+      console.log(NUM_SEPARATORS);
+      console.log(index);
+
+      console.log('Item Number', NUM_SEPARATORS + index + 1);
+
+      let positionY =
+        positionChange > 0
+          ? (index + NUM_SEPARATORS) * ITEM_HEIGHT
+          : positionChange < 0
+          ? (index - 1 + NUM_SEPARATORS) * ITEM_HEIGHT
+          : (index + 1 + NUM_SEPARATORS) * ITEM_HEIGHT;
+
+      setModalDetails({
+        id: item.id,
+        index: index,
+        completed: item.completed,
+        title: item.title,
+        planned: item.type !== 'unplanned',
+        x: 0,
+        y: positionY,
+      });
+    };
+
+    const handleClickAwayPress = () => {
+      if (modalDetails) {
+        setModalDetails(null);
+      }
     };
 
     return (
-      <View style={{ zIndex: item.id === openModalId ? 1 : 0 }}>
+      <Pressable style={{ height: ITEM_HEIGHT }} ref={buttonRef} onPress={handleClickAwayPress}>
         <Pressable onPress={handlePress} style={{ ...styles.pressable, left: item.x + center }}>
           <View style={{ ...styles.box }}>
             <View style={styles.boxBase}>
@@ -100,13 +155,13 @@ const Journey = () => {
                 style={{
                   ...styles.boxBase,
                   backgroundColor: item.completed ? '#E66642' : '#8AA1B130',
-                  shadowOffset: {
-                    width: 3,
-                    height: 4,
-                  },
-                  shadowRadius: 0,
-                  shadowOpacity: 1,
-                  shadowColor: item.completed ? `#E6664250` : '#8AA1B110',
+                  // shadowOffset: {
+                  //   width: 3,
+                  //   height: 4,
+                  // },
+                  // shadowRadius: 0,
+                  // shadowOpacity: 1,
+                  // shadowColor: item.completed ? `#E6664250` : '#8AA1B110',
                 }}>
                 <View style={styles.boxLeft}>
                   <Icon color={item.completed ? null : '#8AA1B190'} />
@@ -137,24 +192,7 @@ const Journey = () => {
             </View>
           </View>
         </Pressable>
-        {item.id === openModalId && (
-          <View style={{ ...styles.pointerBorder, borderBottomColor: item.completed ? '#E66642' : '#8AA1B130' }} />
-        )}
-        {item.id === openModalId && (
-          <View style={{ ...styles.pointerFill, borderBottomColor: item.completed ? '#E66642' : '#16171B' }} />
-        )}
-        {item.id === openModalId && (
-          <View
-            style={{
-              ...styles.callout,
-              width: screenWidth - 40,
-              backgroundColor: item.completed ? '#E66642' : '#16171B',
-              borderColor: item.completed ? '#E66642' : '#8AA1B130',
-            }}>
-            <Text style={styles.modalHeader}>{item.title}</Text>
-          </View>
-        )}
-      </View>
+      </Pressable>
     );
   };
 
@@ -162,12 +200,18 @@ const Journey = () => {
     let weekNumber = 0;
     const date = moment.utc(item.leadingItem?.date, 'YYYY-MM-DD').format('YYYY-MM-DD');
 
+    const handleClickAwayPress = () => {
+      if (modalDetails) {
+        setModalDetails(null);
+      }
+    };
+
     if (startOfWeekDates.includes(date)) {
       weekNumber = startOfWeekDates.indexOf(date) + 1;
       const focus = user.weeklyFocuses[`week ${weekNumber}`];
 
       return (
-        <View style={styles.separatorContainer}>
+        <Pressable onPress={handleClickAwayPress} style={styles.separatorContainer}>
           <View style={styles.separatorTop}>
             <View style={styles.separatorDiv} />
             <Text style={styles.separatorTitle}>{`Week ${weekNumber}`}</Text>
@@ -176,7 +220,7 @@ const Journey = () => {
           <View style={styles.separatorBody}>
             <Text style={styles.separatorBodyText}>{focus || ''}</Text>
           </View>
-        </View>
+        </Pressable>
       );
     }
   };
@@ -196,26 +240,57 @@ const Journey = () => {
   return (
     <ImageBackground source={background} resizeMode="cover" style={{ flex: 1 }}>
       <Top month={activeMonth} />
-      <FlatList
-        ref={flatListRef}
-        style={{ marginTop: 4 }}
-        contentContainerStyle={{ paddingTop: 35 }}
-        onLayout={handleLayout}
-        data={plannedActivities}
-        onScroll={handleScroll}
-        onScrollToIndexFailed={({ index, averageItemLength }) => {
-          flatListRef.current?.scrollToOffset({
-            offset: index * averageItemLength,
-            animated: true,
-          });
-        }}
-        renderItem={renderItem}
-        windowSize={10}
-        ItemSeparatorComponent={(item, index) => <Separator item={item} index={index} />}
-        keyExtractor={(item) => item.id}
-        onViewableItemsChanged={onViewableItemsChanged}
-        viewabilityConfig={viewabilityConfig}
-      />
+      <View style={{ marginTop: 4, marginBottom: TAB_HEIGHT }}>
+        {modalDetails && (
+          <View
+            style={{
+              ...styles.pointerBorder,
+              top: modalDetails.y,
+              borderBottomColor: modalDetails.completed ? '#E66642' : '#8AA1B130',
+            }}
+          />
+        )}
+        {modalDetails && (
+          <View
+            style={{
+              ...styles.pointerFill,
+              borderBottomColor: modalDetails.completed ? '#E66642' : '#16171B',
+              top: modalDetails.y + 3,
+            }}
+          />
+        )}
+        {modalDetails && (
+          <View
+            style={{
+              ...styles.callout,
+              width: screenWidth - 40,
+              top: modalDetails.y + 8,
+              backgroundColor: modalDetails.completed ? '#E66642' : '#16171B',
+              borderColor: modalDetails.completed ? '#E66642' : '#8AA1B130',
+            }}>
+            <Text style={styles.modalHeader}>{modalDetails.title}</Text>
+          </View>
+        )}
+        <FlatList
+          ref={flatListRef}
+          contentContainerStyle={{ paddingTop: 35 }}
+          onLayout={handleLayout}
+          data={plannedActivities}
+          onScroll={handleScroll}
+          onScrollToIndexFailed={({ index, averageItemLength }) => {
+            flatListRef.current?.scrollToOffset({
+              offset: index * averageItemLength,
+              animated: true,
+            });
+          }}
+          renderItem={(item) => <ListItem {...item} />}
+          windowSize={10}
+          ItemSeparatorComponent={(item, index) => <Separator item={item} index={index} />}
+          keyExtractor={(item) => item.id}
+          onViewableItemsChanged={onViewableItemsChanged}
+          viewabilityConfig={viewabilityConfig}
+        />
+      </View>
     </ImageBackground>
   );
 };
@@ -239,13 +314,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#16171B',
     borderRadius: 12,
-    shadowOffset: {
-      width: 3,
-      height: 4,
-    },
-    shadowRadius: 0,
-    shadowOpacity: 1,
-    shadowColor: '#16171B',
+    // shadowOffset: {
+    //   width: 3,
+    //   height: 4,
+    // },
+    // shadowRadius: 0,
+    // shadowOpacity: 1,
+    // shadowColor: '#16171B',
   },
   boxLeft: {
     flex: 0.35,
@@ -263,6 +338,7 @@ const styles = StyleSheet.create({
   },
   separatorContainer: {
     width: '100%',
+    height: ITEM_HEIGHT,
   },
   separatorTop: {
     flexDirection: 'row',
@@ -287,7 +363,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingHorizontal: 20,
     marginTop: 8,
-    marginBottom: 35,
   },
   separatorBodyText: {
     color: '#8AA1B170',
@@ -298,10 +373,9 @@ const styles = StyleSheet.create({
   },
   callout: {
     position: 'absolute',
-    top: BOX_HEIGHT + 10,
     backgroundColor: 'orange',
     padding: 10,
-    borderRadius: 6,
+    borderRadius: 12,
     marginHorizontal: 20,
     zIndex: 1,
     borderWidth: 2,
