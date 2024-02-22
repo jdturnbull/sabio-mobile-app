1e719dcefffbb0ae57e0db08cdcf9adc429dfb4e
Jazz Turnbull <jdturnbull98@gmail.com>
2024-02-22 18:13:59 +0000

    .

--- app/config.js
+++ app/config.js
@@ -10,6 +10,6 @@ const CONFIG_NGROK = {
   API_BASE: 'https://c7ac-2a00-23c7-b280-9801-7d82-b949-c3c4-ad18.ngrok-free.app',
 };
 
-const CONFIG = process.env.NODE_ENV === 'production' ? CONFIG_NGROK : CONFIG_DEV;
+const CONFIG = process.env.NODE_ENV === 'production' ? CONFIG_PROD : CONFIG_DEV;
 
 export const apiBase = CONFIG.API_BASE;
